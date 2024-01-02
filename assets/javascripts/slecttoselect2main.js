b5ac648c195e48d09b7da1caa67610da6bbdc396
Jethro Yu <jethro@teamt5.org>
2024-01-02 15:22:14 +0800

    refactor(select2main.js): Enhance event handling and selective replacements in select2 elements

--- assets/javascripts/slecttoselect2main.js
+++ assets/javascripts/slecttoselect2main.js
@@ -2,65 +2,84 @@
  Select To Select2 Plugin Main.
  */
 
-(function($){
-
-    // For Page Load
+(function ($) {
+  // For Ajax
+  $(document).on("ajaxComplete", function () {
     replaceAllSelect2();
+  });
 
-    // For Ajax
-    $(document).ajaxComplete(function(event){
-        replaceAllSelect2();
-    });
+  // For GET,POST Request
+  // For jQuery Version >= 3.X Redmine Version 5.0.2 4.2.7 3.4.13 OK
+  $(window).on("load", function () {
+    replaceAllSelect2();
+  });
 
-    // For GET,POST Request
-    // For jQuery Version >= 3.X Redmine Version 5.0.2 4.2.7 3.4.13 OK
-    $(window).on('load',function() {
-        replaceAllSelect2();
-    });
+  $("div").on("change", "select", function (event) {
+    replaceAllSelect2();
+  });
 
-    // for all elements
-    // $(document).click(function(event){
-    //     replaceAllSelect2();
-    // });
+  // Judge and process the class name for some special buttons.
+  $(document).on("click", function (event) {
+    /**
+     * "toggle-multiselect" is a class name of toggle multi select elements
+     * "assign-to-me-link" is a class name of assign to me link
+     */
+    // var hasClass = Array.from(event.target.classList).indexOf('name') > -1;
 
-    // for all elements
-    $(document).change(function(event){
-        replaceAllSelect2();
-    });
+    if (
+      $(event.target).hasClass("toggle-multiselect") ||
+      $(event.target).hasClass("assign-to-me-link") ||
+      $(event.target).hasClass("icon-expended")
+    ) {
+      replaceAllSelect2();
+    }
+  });
 
+  // for all elements
+  $(document).change(function (event) {
+    replaceAllSelect2();
+  });
+})(jQuery);
 
-}(jQuery));
+function replaceAllSelect2() {
+  var ignoreIds = [
+    "available_c",
+    "selected_c",
+    "issue_template",
+    "issue_assigned_to_id",
+  ];
+  var specificIds = [
+    "year",
+    "month",
+    "columns",
+    "settings_issuequery_query_id",
+    "block-select",
+  ];
 
-function replaceAllSelect2(){
+  var elements = $("select");
 
-    var elements = document.getElementsByTagName("select");
-
-    for (i = 0; i < elements.length; i++) {
-
-        // For not woroking 「width:resolve」
-        if(elements[i].id == 'year'
-        || elements[i].id == 'month'
-        || elements[i].id == 'columns'
-        || elements[i].id == 'settings_issuequery_query_id'
-        || elements[i].id == 'block-select'){
-
-            $("#" + elements[i].id).select2({
-                width:"175px",
-                placeholder: "",
-                allowClear: true
-            });
-        }
-        else if (elements[i].id == 'available_c' || elements[i].id == 'selected_c') {
-            //do nothing, fix incorrect display
-        } else {
-            // For All Pages
-            $("#" + elements[i].id).select2({
-                width:"resolve",
-                placeholder: "",
-                allowClear: true
-            });
-        }
-
+    // Skip if the ID starts with "values_" or it's in the ignore list or already a Select2
+    if (id.startsWith("values_") || ignoreIds.includes(id)) {
+      return;
     }
 
+    // Apply specific width for certain IDs
+    if (specificIds.includes(id)) {
+      $this.select2({
+        dropdownAutoWidth: true,
+        width: "175px",
+        placeholder: "",
+        allowClear: true,
+      });
+    } else {
+      // Apply general settings for all other select elements
+      var w = $this.width();
+      $this.select2({
+        dropdownAutoWidth: true,
+        width: "resolve",
+        placeholder: "",
+        allowClear: true,
+      });
+    }
+  }
 }
